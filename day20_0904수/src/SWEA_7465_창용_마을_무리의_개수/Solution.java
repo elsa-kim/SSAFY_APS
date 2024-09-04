167a829293f82d2232b21635eb18f71e3c06d4dd
elsa-kim <ksh96611@naver.com>
2024-09-04 16:33:13 +0900

    9/4

---
+++ day20_0904수/src/SWEA_7465_창용_마을_무리의_개수/Solution.java
@@ -0,0 +1,50 @@
+package SWEA_7465_창용_마을_무리의_개수;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+	static int p, line, muri;
+	static ArrayList<Integer>[] info;
+	static boolean[] check;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			p = sc.nextInt();
+			line = sc.nextInt();
+			info = new ArrayList[p + 1];
+			muri = 0;
+			for (int i = 0; i < p + 1; i++) {
+				info[i] = new ArrayList<>();
+			}
+			for (int i = 0; i < line; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				info[a].add(b);
+				info[b].add(a);
+			}
+			check = new boolean[p + 1];
+			for (int i = 1; i < p + 1; i++) {
+				if (check[i])
+					continue;
+				muri++;
+				getMuri(i);
+			}
+
+			System.out.println("#" + tc + " " + muri);
+
+		}
+	}
+
+	private static void getMuri(int i) {
+		for (int k = 0; k < info[i].size(); k++) {
+			if (check[info[i].get(k)])
+				continue;
+			check[info[i].get(k)] = true;
+			getMuri(info[i].get(k));
+		}
+
+	}
+}
